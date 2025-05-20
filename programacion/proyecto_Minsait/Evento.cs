5f3e6aee654498b16131d477623f3a5224e98d60
Carlos Teran Sanchez <catesa@alumnatflorida.es>
2025-05-20 14:03:07 +0200

    proc1
    Primera consolidacion, creacion de clases, atributos y algunos metodos.

---
+++ programacion/proyecto_Minsait/Evento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_Minsait
+{
+    class Evento
+    {
+        // Atributos
+        private int id;
+        private string nombre;
+        private DateTime fecha;
+        private TimeSpan duracion;
+        private Categoria categoria;
+        private Ubicacion ubicacion;
+        private Organizador organizador;
+        private bool cancelado;
+        private List<Usuario> usuarios;
+
+        // Constructor
+        public Evento(int id, string nom, DateTime fecha, TimeSpan dura, Categoria cate, Ubicacion ubi)
+        {
+            this.id = id;
+            nombre = nom;
+            this.fecha = fecha;
+            this.duracion = dura;
+            this.categoria = cate;
+            this.ubicacion = ubi;
+            usuarios = new List<Usuario>();
+            cancelado = false;
+        }
+
+        // Metodos 
+
+        public void RegidtrarUsuario(Usuario usu)
+        {
+            
+           if (!cancelado && usuarios.Contains(usu))
+           {
+                usuarios.Add(usu);
+           }
+            
+        }
+
+        public void CancelarEvento()
+        {
+            cancelado = true;
+            usuarios.Clear();
+        }
+
+        public List<Usuario> ObtenerUsuarios()
+        {
+            return usuarios;
+        }
+
+
+
+    }
+}
